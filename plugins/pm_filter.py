107f0705a16cb8550732e0c8c624cc7f448d9726
Favazmp1 <119835163+Favazmp@users.noreply.github.com>
2022-12-14 17:10:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -129,6 +129,8 @@ async def next_page(bot, query):
         )
     btn.insert(0, [
         InlineKeyboardButton(text="🔰 𝗠𝗢𝗩𝗜𝗘 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🔰", url="https://t.me/movieupdatestalkiez")
+    ],[
+        InlineKeyboardButton(text="🔰 𝗠𝗢𝗩𝗜𝗘 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🔰", url="https://t.me/movieupdatestalkiez")
     ])
     try:
         await query.edit_message_reply_markup(
@@ -692,6 +694,8 @@ async def auto_filter(client, msg, spoll=False):
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
         )
     btn.insert(0, [
+        InlineKeyboardButton(text="🔰 𝗠𝗢𝗩𝗜𝗘 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🔰", url="https://t.me/movieupdatestalkiez")
+    ],[
         InlineKeyboardButton(text="🔰 𝗠𝗢𝗩𝗜𝗘 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 🔰", url="https://t.me/movieupdatestalkiez")
     ])
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
