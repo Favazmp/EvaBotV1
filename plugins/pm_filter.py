66eefe22fa35f8346a26711ac29b37769b931d94
Favazmp1 <119835163+Favazmp@users.noreply.github.com>
2023-02-02 06:29:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("🤒 𝐇𝐞𝐲 𝐅𝐫𝐢𝐞𝐧𝐝, 𝐓𝐡𝐢𝐬 𝐈𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲 𝐎𝐭𝐡𝐞𝐫𝐬.𝐏𝐥𝐞𝐚𝐬𝐞 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐘𝐨𝐮𝐫𝐬𝐞𝐥𝐟...🙂", show_alert=True)
     try:
         offset = int(offset)
     except:
