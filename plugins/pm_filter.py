15ee7be1973341bba2c73407c08354706718d565
Favazmp1 <119835163+Favazmp@users.noreply.github.com>
2023-02-19 19:17:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -807,7 +807,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply(" <a href=https://t.me/rulesandregulation/6><b></b>ɢʀᴏᴜᴘ ꜱᴇᴀʀᴄʜ ʀᴜʟᴇꜱ</a> \n I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply(" 𝗥𝗲𝗮𝗱: <a href=https://t.me/rulesandregulation/6><b></b>ɢʀᴏᴜᴘ ꜱᴇᴀʀᴄʜ ʀᴜʟᴇꜱ</a> \n I couldn't find anything related to that. \nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
