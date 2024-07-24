60a07b22dc8beb1bdde95296c56af1daac7ec9c2
Favazmp1 <119835163+Favazmp@users.noreply.github.com>
2024-07-24 07:11:00 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/movieupdatestalkiez')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/MT_Linkz')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
